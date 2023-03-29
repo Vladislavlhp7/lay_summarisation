981e55207510061fc2c8d0d8074b8ae907cedb07
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2023-03-29 14:59:37 +0100

    Add download scripts

---
+++ scripts/prep_all.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+./prep_weights.sh
+./prep_data.sh
---
+++ scripts/prep_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Set the download URL and output file name
+DOWNLOAD_URL="https://drive.google.com/uc?id=1FFfa4fHlhEAyJZIM2Ue-AR6Noe9gOJOF&export=download&confirm=t&uuid=01b721da-12a0-46cd-a5b4-9ce1620a4d1e"
+OUTPUT_FILE="assets/task1_development.zip"
+
+# Set the output directory for extracted files
+OUTPUT_DIR="assets"
+mkdir -p $OUTPUT_DIR
+
+# Download the file using curl command
+curl -L -o $OUTPUT_FILE $DOWNLOAD_URL
+
+# Extract the contents of the zip file
+unzip $OUTPUT_FILE -d $OUTPUT_DIR
+
+# Delete the zip file
+rm $OUTPUT_FILE
