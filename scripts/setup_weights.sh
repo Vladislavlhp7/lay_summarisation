fc4f78b3786c29d0a5e668f1fd6e4ff22f5b16cb
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2023-04-10 16:16:00 +0100

    fix weight downloading

--- scripts/setup_weights.sh
+++ scripts/setup_weights.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-# Setup the clinical longformer model
+mkdir -p weights
+
+# Download the clinical longformer model weights
 git lfs install
 git clone https://huggingface.co/yikuan8/Clinical-Longformer ./weights/Clinical-Longformer
