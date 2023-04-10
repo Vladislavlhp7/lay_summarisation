ce3205660f2be535312b8dce6abd867cd5617077
Vladislav Yotkov <vladislav.yotkov@student.manchester.ac.uk>
2023-04-10 14:41:07 +0300

    add script for running finetuning on CSF

---
+++ scripts/fine_tune_longformer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash --login
+#$ -N clinical_longformer_
+#$ -cwd
+
+#$ -l v100
+#$ -pe smp.pe 8
+
+module load libs/cuda
+module load apps/binapps/pytorch/1.11.0-39-gpu-cu113
+module load apps/binapps/anaconda3/2021.11  # Python 3.9.7
+
+python3 ../laysummarisation/model/clinical_longformer.py
