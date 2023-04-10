d730227d0c6f5d29c4c6afebd1ce5ee0745a31d0
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2023-04-10 16:12:06 +0100

    minor fix

--- scripts/setup_data.sh
+++ scripts/setup_data.sh
@@ -2,14 +2,14 @@
 
 # Set the download URL and output file name
 DOWNLOAD_URL="https://drive.google.com/uc?id=1FFfa4fHlhEAyJZIM2Ue-AR6Noe9gOJOF&export=download&confirm=t&uuid=01b721da-12a0-46cd-a5b4-9ce1620a4d1e"
-OUTPUT_FILE="data/task1_development.zip"
+OUTPUT_FILE="./data/task1_development.zip"
 
 # Set the output directory for extracted files
-OUTPUT_DIR="data/orig"
+OUTPUT_DIR="./data/orig"
 mkdir -p $OUTPUT_DIR
 
 # cleanup
-rm -rf $OUTPUT_DIR/task1_development
+rm -rf $OUTPUT_DIR
 
 # Download the file using curl command
 curl -L -o $OUTPUT_FILE $DOWNLOAD_URL
