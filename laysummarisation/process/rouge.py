ab80b39b9a744140eb49e85f9852d7b538b7a7bc
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2023-04-17 12:56:28 +0100

    update dependencies

--- laysummarisation/process/rouge.py
+++ laysummarisation/process/rouge.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Optional, List
 
 from transformers import HfArgumentParser
+import pandas as pd
 
 from laysummarisation.utils import lexrank_summarize, load_jsonl_pandas
 
@@ -18,26 +19,57 @@ class Arguments:
     output: str = field(
         metadata={"help": "The output mrp file path"},
     )
-    lex_sent: Optional[int] = field(
+    rouge_sent: Optional[int] = field(
         default=25,
         metadata={"help": "The number of sentences to extract from the article."},
     )
-    entries: Optional[int] = field(
+    nrows: Optional[int] = field(
         default=None,
         metadata={"help": "The number of entries to process."},
     )
 
 
+def rouge_maximise(corpus, sentence):
+    """
+    Compute the rouge f1 metric for each sentence in the corpus and return the array
+    of scores.
+    """
+    scores = []
+    rouge = Rouge()
+    for i, c in enumerate(corpus):
+        scores.append(rouge.get_scores(c, sentence, avg=True)["rouge-l"]["f"])
+    return np.array(scores)
+
+
+def rouge_lay_sent(corpus: List[str], lay: List[str]):
+    """
+    Compute the rouge metric for each sentence in the lay, for each sentence in the summary
+    """
+    scores = []
+    for l in lay:
+        scores.append(rouge_maximise(corpus, l))
+    return scores
+
+
 def main(conf: Arguments):
-    raise NotImplementedError("Rouge maximisation preprocessing not implemented yet")
+    # raise NotImplementedError("Rouge maximisation preprocessing not implemented yet")
     # Load files
     print("Loading files...")
-    data = load_jsonl_pandas(conf.fname, nrows=conf.entries)
+    data = load_jsonl_pandas(conf.fname, nrows=conf.nrows)
 
     # LexRank summarise the articles
     print("Summarising articles...")
-    data["article"] = data["article"].apply(lexrank_summarize)
+    data["article"] = data["article"].apply(rouge_summarize, args=(conf.rouge_sent,))
 
+    with open("tmp.txt", "r") as f:
+        text = f.read()
+
+    data = pd.read_json("data/orig/train/eLife_train.jsonl", lines=True, nrows=1)
+    point = data.iloc[0]
+    r = Rouge()
+
+    rl = r.get_scores(text, point.lay_summary, avg=True)
+    print(rl)
     # Save the data
     print("Saving data...")
     data.to_json(conf.output, orient="records", lines=True)
