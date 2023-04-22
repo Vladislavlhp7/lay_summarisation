9ab922f8c92a2851404cc4dade962a218008cf8a
Vladislav Yotkov <vladislav.yotkov@student.manchester.ac.uk>
2023-04-22 22:11:33 +0100

    fix sh file

--- scripts/data/binary_sentences.sh
+++ scripts/data/binary_sentences.sh
@@ -4,8 +4,8 @@
 SEED_DIR=./data/tmp/extractive/${ALGO}
 mkdir -p ${SEED_DIR}
 
-python -m laysummarisation.process.greedy_rouge \
-    --summary_fname ./data/${ALGO}/${CORPUS}_train.jsonl \
-    --fname ./data/orig/train/${CORPUS}_train.jsonl \
+python -m laysummarisation.process.binary_sentences \
+    --summary_fname ./data/input/${ALGO}/${CORPUS}_train.jsonl \
+    --article_fname ./data/orig/train/${CORPUS}_train.jsonl \
     --output ${SEED_DIR}/${CORPUS}_train.jsonl \
     --narticles 10000000
