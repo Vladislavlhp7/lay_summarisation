f8d20b93144138438b9955fa569a9c730c61de3d
Vladislav Yotkov <vladislav.yotkov@student.manchester.ac.uk>
2023-04-23 15:05:02 +0100

    add a binary df sh script

--- scripts/data/binary_sentences.sh
+++ scripts/data/binary_sentences.sh
@@ -7,5 +7,5 @@ mkdir -p ${SEED_DIR}
 python -m laysummarisation.process.binary_sentences \
     --summary_fname ./data/input/${ALGO}/${CORPUS}_train.jsonl \
     --article_fname ./data/orig/train/${CORPUS}_train.jsonl \
-    --output ${SEED_DIR}/${CORPUS}_train.jsonl \
-    --narticles 10000000
+    --output ${SEED_DIR}/${CORPUS}_train.csv \
+    --narticles 2
