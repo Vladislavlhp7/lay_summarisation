f0bcea4180fd584e3835ab11fdaeb93f05e8b5e7
Vladislav Yotkov <vladislav.yotkov@student.manchester.ac.uk>
2023-04-24 20:02:29 +0100

    enable reading of scripts

--- scripts/process_binary_sentences.sh
+++ scripts/process_binary_sentences.sh
@@ -6,6 +6,6 @@ export CORPUS=eLife
 export ALGO=rouge
 ./scripts/data/binary_sentences.sh
 
-export CORPUS=PLOS
-export ALGO=rouge
-./scripts/data/binary_sentences.sh
+#export CORPUS=PLOS
+#export ALGO=rouge
+#./scripts/data/binary_sentences.sh
