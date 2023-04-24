62ae8bbe082333ad93e42ad589f7ec73fd3209a0
Marc Wenzlawski <marc.wenzlawski@outlook.de>
2023-04-24 19:24:43 +0100

    add comment

--- laysummarisation/process/greedy_rouge.py
+++ laysummarisation/process/greedy_rouge.py
@@ -130,6 +130,8 @@ if __name__ == "__main__":
     nltk.download("punkt")
     parser = HfArgumentParser(Arguments)
     conf = parser.parse_args_into_dataclasses()[0]
+
+    # INFO: ZERO LOADS ALL LINES
     if conf.nrows == 0:
         conf.nrows = None
 
