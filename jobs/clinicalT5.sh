f1ed7a7a63abb406218c48286deddd1384870dd8
Ahmed Soliman <ahmedsoliman360@gmail.com>
2023-04-26 16:51:40 +0100

    Add T5 Job and Script

---
+++ jobs/clinicalT5.sh
@@ -0,0 +1,17 @@
+#!/bin/bash --login
+#$ -N clinicalT5
+#$ -cwd
+
+#$ -l v100
+#$ -pe smp.pe 8
+
+module load libs/cuda
+module load apps/binapps/pytorch/1.11.0-39-gpu-cu113
+module load apps/binapps/anaconda3/2021.11  # Python 3.9.7
+module load libs/nvidia-hpc-sdk/23.1
+module load compilers/gcc/9.3.0
+
+export OMP_NUM_THREADS=$NSLOTS
+source activate nlu
+./scripts/train/clinicalT5.sh
+
---
+++ scripts/train/clinicalT5.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# SEED_DIR=./data/input/rouge
+# MODEL="clinicalT5"
+# CORPUS="eLife"
+# SEED=42
+
+python -m laysummarisation.model.clinicalT5
+
+	# --ftrain "${SEED_DIR}/${CORPUS}_train.jsonl" \
+	# --fvalid "${SEED_DIR}/${CORPUS}_val.jsonl" \
+	# --corpus ${CORPUS} \
+	# --save_dir tmp/gpt2 \
+	# --seed ${SEED} \
+	# --model_checkpoint ${MODEL} \
+	# --device "cuda" \
+	# --temperature 0.7 \
+	# --max_encode 1024 \
+	# --lr 1e-5 \
+	# --batch_size 1 \
+	# --epochs 3 \
+	# --save_steps 2000
